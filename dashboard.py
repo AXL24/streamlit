e0733c1ba344889cdd399cd140ed3479abb7299b
quang <axellent2004@gmail.com>
2024-11-17 13:47:17 +0700

    sd

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,13 @@ from merge import prepare
 import pandas as pd
 import plotly.express as px
 
+def main():
+    st.title("Phân Tích Điểm THPT 2022-2024")
+    combined_data= prepare()
+    df =pd.DataFrame(combined_data)
+    st.subheader("First 10 Records")
+    st.dataframe(df.head(10))
 
-combined_data= prepare()
-df =pd.DataFrame(combined_data)
-print(df.head(10))
+
+if __name__ == "__main__":
+    main()
