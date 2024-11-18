fa6bbda2e46110b110d86ea12cc993b2151a36be
quang <axellent2004@gmail.com>
2024-11-18 19:00:26 +0700

    str

--- merge.py
+++ merge.py
@@ -13,14 +13,20 @@ def fetch_data(db, collection_name, year_filter=None):
         query = {"nam": {"$in": year_filter}}  # Lọc theo năm
 
     # Lấy chỉ các cột cần thiết
-    projection = {
-        "sbd": 1, "toan": 1, "ngu_van": 1, "ngoai_ngu": 1,
-        "vat_li": 1, "hoa_hoc": 1, "sinh_hoc": 1, "lich_su": 1,
-        "dia_li": 1, "gdcd": 1, "_id": 0
-    }
+    pipeline = [
+        {
+            "$project": {
+                "sbd": {"$toString": "$sbd"},  # Convert sbd to string
+                "toan": 1, "ngu_van": 1, "ngoai_ngu": 1,
+                "vat_li": 1, "hoa_hoc": 1, "sinh_hoc": 1,
+                "lich_su": 1, "dia_li": 1, "gdcd": 1,
+                "_id": 0
+            }
+        }
+    ]
 
     # Truy vấn dữ liệu MongoDB
-    records = db[collection_name].find(query, projection)
+    records = db[collection_name].find(query, pipeline)
     df = pd.DataFrame(records)
 
     # Chuẩn hóa tên cột
