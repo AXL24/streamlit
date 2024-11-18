0aef447f16de292889b91b4b1b5b82a6872ffc47
quang <axellent2004@gmail.com>
2024-11-18 21:12:25 +0700

    hgvm

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,6 @@ DATABASE_NAME = "thpt"
 # Caching để tăng hiệu suất
 @st.cache_data
 def load_data():
-    
     # Kết nối tới MongoDB
     db = connect_to_mongodb(MONGO_URI, DATABASE_NAME) 
     
@@ -30,8 +29,11 @@ def load_data():
 
 # Tạo ứng dụng Streamlit
 def main():
-    st.title("Phân Tích Điểm THPT - 2023 & 2024\n sử dụng cloud mongodb")
-    combined_data = load_data()
+    st.title("Phân Tích Điểm THPT - 2023 & 2024\nsử dụng cloud mongodb")
+    
+    # Tải dữ liệu
+    with st.spinner("Đang tải dữ liệu..."):
+        combined_data = load_data()
     
     # Sidebar: Bộ lọc
     st.sidebar.header("Bộ lọc")
@@ -50,14 +52,9 @@ def main():
     filtered_data = combined_data[combined_data["nam"].isin(cac_nam)]
     
     # Hiển thị bảng dữ liệu
-    st.subheader("Dữ liệu tham khảo")
+    st.subheader("Dữ liệu đã lọc")
     st.write(f"Tổng số bản ghi: {len(filtered_data)}")
     st.dataframe(filtered_data.head(100))  # Hiển thị 100 bản ghi đầu tiên
-
-    # Phân tích điểm trung bình theo môn học
-    if not filtered_data.empty:
-        # Search functionality
-        st.subheader("Tìm kiếm theo số báo danh")
     if roll_number:
         search_results = combined_data[(combined_data["nam"] == selected_year) & 
                                        (combined_data["sbd"] == roll_number)]
@@ -68,6 +65,14 @@ def main():
         else:
             st.warning(f"Không tìm thấy kết quả cho số báo danh: {roll_number} năm {selected_year}")
 
+
+
+
+    # Phân tích điểm trung bình theo môn học
+    if not filtered_data.empty:
+        # Search functionality
+        st.subheader("Tìm kiếm theo số báo danh")
+   
 
 
         st.subheader("Điểm trung bình theo môn học của năm đã chọn")
